db83f2c68c3c2eed7b17f826a18efab5a04d3426
Jeff Jennings <jjennings1519@gmail.com>
2024-02-14 23:51:06 -0500

    assemble, compile instructions

--- hello_world.s
+++ hello_world.s
@@ -3,3 +3,8 @@
 # step 0 - install assembler 'as' and linker 'ld' to compile 
 # assembly code into machine code:
 # sudo apt install gcc
+# step 1 - assemble this script via: 
+# as hello_world.s -o hello_world.o
+# step 2 - compile this script 
+# (invoke linker to convert *.o into an executable ELF) via:
+# gcc -o hello_world hello_world.o -nostdlib -static
